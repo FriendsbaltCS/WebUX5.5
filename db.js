c8f4cb7f0f24918c6753e10c780e6dd6fc67ef83
jahAtFriends <jhammer@friendsbalt.org>
2024-03-07 23:47:49 +0000

    add docstrings

--- db.js
+++ db.js
@@ -1,6 +1,34 @@
+/**
+ * 
+ * 
+ * Database helper functions for user authentication and registration.
+ * This module is designed to assist students of Web and the User Experience.
+ * 
+ * (c) 2024 Joel Hammer
+ * Friends School of Baltimore
+ * Department of Computer Science
+ * 
+ * @module db
+ * @requires sqlite3
+ * @requires bcrypt
+ * 
+ * 
+ * This module is published under the MIT License.
+ * For more information, see the LICENSE file in this repository.
+ * 
+ * 
+ * @example
+ * const db = require('./db');
+ */
+
+
 const sqlite3 = require('sqlite3').verbose();
 const bcrypt = require('bcrypt');
 
+
+/**
+ * Initialize the database to store basic user information.
+ */
 async function initializeDatabase() {
     const db = new sqlite3.Database('database.db', (err) => {
         if (err) {
@@ -25,6 +53,14 @@ async function initializeDatabase() {
     });
 }
 
+
+/**
+ * Authenticates a user by checking their username and password against the database.
+ * @param {Object} user - The user object containing the username and password.
+ * @param {string} user.username - The username of the user.
+ * @param {string} user.password - The password of the user.
+ * @returns {Promise<Object|null>} - A promise that resolves with the user object if authentication is successful, or null if authentication fails.
+ */
 async function authenticateUser(user) {
     const { username, password } = user;
     const db = new sqlite3.Database('database.db');
@@ -54,6 +90,16 @@ async function authenticateUser(user) {
     });
 }
 
+/**
+ * Registers a new user in the database.
+ * @param {Object} user - The user object containing user details.
+ * @param {string} user.firstName - The first name of the user.
+ * @param {string} user.lastName - The last name of the user.
+ * @param {string} user.email - The email address of the user.
+ * @param {string} user.username - The username of the user.
+ * @param {string} user.password - The password of the user.
+ * @returns {Promise<void>} - A promise that resolves when the user is registered.
+ */
 async function registerUser(user) {
     const { firstName, lastName, email, username, password } = user;
     const hashedPassword = await bcrypt.hash(password, 10);
