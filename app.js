ae5d31629819ea413d7ad9230beb7c1c54289e8b
jahAtFriends <jhammer@friendsbalt.org>
2024-03-07 17:58:57 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
